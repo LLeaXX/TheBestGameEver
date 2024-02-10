ad7933427224d0d01dbd7f6378960d0aa50f8150
LLeaXX <se599482@gmail.com>
2024-02-10 22:08:50 +0800

    He's moving!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,25 +10,57 @@ public class PlayerController : MonoBehaviour
 
     private float _fallVelocity = 0;
 
+    public float speed;
+
+    private Vector3 _moveVector;
+
     private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+       
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _fallVelocity += gravity * Time.fixedDeltaTime; 
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
     }
 
     void Update()
     {
+        _moveVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded) 
         {
             _fallVelocity = -jumpForce;
+        }
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
         }
     }
 }   
