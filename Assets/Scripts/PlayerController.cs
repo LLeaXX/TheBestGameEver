bac44b96015e94c654c0bba4d2e4653458e048fb
LLeaXX <se599482@gmail.com>
2024-02-10 20:54:26 +0800

    He's able to jump now!!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+
+    public float jumpForce;
+
+    private float _fallVelocity = 0;
+
+    private CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded) 
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
+}   
