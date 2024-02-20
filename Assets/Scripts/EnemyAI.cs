f140b027476d52c62b0be7355f4525a520dd3784
LLeaXX <se599482@gmail.com>
2024-02-20 20:55:28 +0800

    Got all done!!

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+
+    public PlayerController player;
+
+    public List<Transform> patrolPoints;
+
+    private bool _isPlayerNoticed;
+
+    public float viewAngle;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    { 
+        PatrolUptade();
+        ChaseUpdate();
+        NoticePlayerUpdate();
+    }
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+
+    }  
+
+    private void PatrolUptade()
+    {
+        if (!_isPlayerNoticed)
+        {
+
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+
+    public float damage = 10;
+
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);  
+        DestroyFireball();
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+    }
+}   
